5ec6b61ee893a408d9d4b1aa05faf172b736b223
David Mosher <davidmosher@gmail.com>
2017-08-26 22:58:07 -0400

    sets sortBy and sortOrder (ASC/DESC)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import React from 'react';
 import './App.css';
 import response from './data.json';
 import _ from 'lodash';
+import { compose, withState, withHandlers } from 'recompose';
 
-const Table = ({ data, cols }) =>
+const withSorting = compose(
+  withState('sortBy', 'updateSort', ''),
+  withState('sortOrder', 'updateSortOrder', ''),
+  withHandlers({
+    toggleSortOrder: ({ sortOrder, updateSortOrder }) => event => {
+      updateSortOrder(sortOrder === '' || sortOrder === 'DESC' ? 'ASC' : 'DESC');
+    },
+    resetSort: ({ updateSort, updateSortOrder }) => event => {
+      updateSort('')
+      updateSortOrder('')
+    },
+  }),
+);
+
+const Table = ({ data, cols, sortBy, updateSort, toggleSortOrder, resetSort }) =>
   <table>
+    {sortBy &&
+      <thead>
+        <tr>
+          <th>
+            <button onClick={resetSort}>Reset Sort</button>
+          </th>
+        </tr>
+      </thead>}
     <thead>
       <tr>
         {cols.map(col =>
-          <th key={col}>
+          <th
+            key={col}
+            onClick={() => {
+              updateSort(col);
+              toggleSortOrder();
+            }}
+          >
             {col}
           </th>,
         )}
@@ -17,7 +46,7 @@ const Table = ({ data, cols }) =>
     <tbody>
       {data.map(row =>
         <tr key={row.id}>
-          {cols.map((col) =>
+          {cols.map(col =>
             <td key={`${row.id}-${col}`}>
               {_.get(row, col)}
             </td>,
@@ -27,19 +56,14 @@ const Table = ({ data, cols }) =>
     </tbody>
   </table>;
 
-const cols = [
-  'name',
-  'address.city',
-  'address.country',
-  'company',
-  'return_date',
-  'return_amount'
-];
+const TableWithSorting = withSorting(Table);
+
+const cols = ['name', 'address.city', 'address.country', 'company', 'return_date', 'return_amount'];
 
 const App = () =>
   <div>
     <h1>Table w/ React & Recompose</h1>
-    <Table data={response.data} cols={cols} />
+    <TableWithSorting data={response.data} cols={cols} />
   </div>;
 
 export default App;
