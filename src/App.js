89defffe1249f7ccf23f10766833d39711a6b224
David Mosher <davidmosher@gmail.com>
2017-08-26 22:06:00 -0400

    setup basic table component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+
+const Table = ({ data, cols }) => (
+  <table>
+    <thead>
+      {cols.map(c => (
+        <tr key={c.header}>
+          <th>{c.header}</th>
+        </tr>
+      ))}
+    </thead>
+    <tbody>
+      {data.map(r => (
+        <tr key={r.id}>
+        {cols.map((c, i) => (
+          <td key={`${r}-${c}${i}`}>{c}</td>
+        ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
 const App = () => (
   <h1>Table w/ React & Recompose</h1>
